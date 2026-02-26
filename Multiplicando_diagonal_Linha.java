09aace6329c7f91f224d023a0c99418b59756cc3
Mika <lovemica700@gmail.com>
2026-02-26 09:58:32 -0300

    Add files via upload

---
+++ Multiplicando_diagonal_Linha.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Multiplicando_diagonal_Linha {
+    
+    final static Scanner ler = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        int[][] matrizC = new 
+    }
+
+}
