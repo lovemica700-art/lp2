dc37ed7ec93cc94bc2f81ba9423de4ab77a071d1
Mika <lovemica700@gmail.com>
2026-08-28 09:00:08 -0300

    Add files via upload

--- Necessito.java
+++ Necessito.java
@@ -1,25 +1,45 @@
+import java.awt.Color;
+import java.awt.event.ItemEvent;
+
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 
 public class Necessito {
 
    public static void main(String[] args) {
 
-     JFrame frame = new JFrame("Exemplo Menu");
+    JFrame frame = new JFrame("Exemplo Mod Escuro");
+    JPanel painel = new JPanel();
        
     JMenuBar barraMenu = new JMenuBar();
 
     JMenu menuExibir = new JMenu("Exibir");
 
+    painel.setBackground(Color.WHITE);
+
     JCheckBoxMenuItem intemModEscuro = new JCheckBoxMenuItem("Mod Escuro");
+
+    intemModEscuro.addItemListener(e ->{
+
+        if (e.getStateChange() == ItemEvent.SELECTED) {
+            painel.setBackground (new Color (45,45,45));
+        }else{
+            painel.setBackground(Color.WHITE);
+        }
+
+        painel.repaint();
+
+    });
     
     menuExibir.add(intemModEscuro);
     barraMenu.add(menuExibir);
 
     
     frame.setJMenuBar(barraMenu);
+    frame.add(painel);
     frame.setSize(400, 300);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
